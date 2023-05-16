81b832858104efab2291d7318653c7989f748e41
ChickenG1ttrDev <123892051+ChickenG1ttrDev@users.noreply.github.com>
2023-05-15 20:04:05 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,6 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+<iframe src="https://chickeng1ttrdev.repl.co"></iframe>
   );
 }
